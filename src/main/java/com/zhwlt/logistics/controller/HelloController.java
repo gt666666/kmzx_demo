9f7cc38080dc542aac5a0b8e7bccbf708f06173b
gt <979627462@qq.com>
2019-08-23 12:43:38 +0800

    内容改变

--- src/main/java/com/zhwlt/logistics/controller/HelloController.java
+++ src/main/java/com/zhwlt/logistics/controller/HelloController.java
@@ -27,7 +27,7 @@ public class HelloController {
     String home() {
         //this.memberService.findAll();
         log.info("********12345");
-        return "www.baidu.com12";
+        return "www.baidu.com123";
     }
 
     @RequestMapping("/echo")
